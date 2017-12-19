af09ea5c5d82a381cd177634233cfbd49d83cd05
Bin <jimmy28year@gmail.com>
2017-12-18 17:09:55 -0800

    Prototype

---
+++ Slices-via-World-Space-Position/Assets/HeightAjust.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class HeightAjust : MonoBehaviour
+{
+    public void HeighAjust(float z)
+    {
+        transform.position = new Vector3(transform.position.x, z, transform.position.z);
+    }
+}
